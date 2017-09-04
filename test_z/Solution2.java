ecf291fa026f2880f5b60db0553b64ae734fe8c1
yongfucius <yongfucius@gmail.com>
2017-09-04 12:11:13 +0900

    Create Solution2.java

---
+++ test_z/Solution2.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution2 {
+
+  // (new int[]{-1, -1, 1, 0, -1, 1, 0, 1, -1, -1}, 2)
+  public int solution(int[] A, int S) {
+    int N = A.length;
+
+    int size = N;
+
+    while (size >= S) {
+      int P = 0;
+      int Q = P + size;
+      for (; Q <= N; P++, Q++) {
+        int sum = Arrays.stream(A, P, Q).sum();
+
+        if (sum == S) return size;
+      }
+
+      size--;
+    }
+
+    return -1;
+  }
+}
